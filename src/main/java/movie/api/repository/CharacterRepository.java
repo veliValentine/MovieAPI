e669860187bd968a3b212f420eeba8430cc988e1
veliValentine <nikkevanukas1995@gmail.com>
2021-02-10 14:49:39 +0200

    feat: character repository! Character missing correct table/column names

---
+++ src/main/java/movie/api/repository/CharacterRepository.java
@@ -0,0 +1,9 @@
+package movie.api.repository;
+
+import movie.api.models.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+}
