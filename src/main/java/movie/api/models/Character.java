2c11a58691887778cf66be6a49a5285ce2eae502
CasaRol <32219634+CasaRol@users.noreply.github.com>
2021-02-10 13:19:48 +0100

    feature: first automatic database creating

---
+++ src/main/java/movie/api/models/Character.java
@@ -0,0 +1,101 @@
+package movie.api.models;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "characters")
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "character_id")
+    private long characterId;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "alias")
+    private String alias;
+
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "character_picture_src")
+    private String characterPictureSrc;
+
+    @ManyToMany
+    @JoinTable(
+            name = "character_movie",
+            joinColumns = {@JoinColumn(name = "character_id")},
+            inverseJoinColumns = {@JoinColumn(name = "movie_id")}
+    )
+    private List<Movie> movies;
+
+    @JsonGetter("movies")
+    public List<String> moviesGetter() {
+        if(movies != null){
+            return movies.stream()
+                    .map(movie -> {
+                        return "/api/v1/movies/" + movie.getMovieId();
+                    }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+
+
+    public Character() {
+    }
+
+    public Character(String fullName, String alias, Gender gender, String characterPictureSrc) {
+        this.fullName = fullName;
+        this.alias = alias;
+        this.gender = gender;
+        this.characterPictureSrc = characterPictureSrc;
+    }
+
+    // Getter and setters
+    public long getId() {
+        return characterId;
+    }
+
+    public void setId(long id) {
+        this.characterId = characterId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getCharacterPictureSrc() {
+        return characterPictureSrc;
+    }
+
+    public void setCharacterPictureSrc(String pictureSrc) {
+        this.characterPictureSrc = pictureSrc;
+    }
+}
---
+++ src/main/java/movie/api/models/Movie.java
@@ -0,0 +1,143 @@
+package movie.api.models;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_id")
+    private long movieId;
+
+    @Column(name = "movie_title")
+    private String movieTitle;
+
+    @Column(name = "director")
+    private String director;
+
+    @Column(name = "release_year")
+    private int releaseYear;
+
+    @OneToMany
+    @JoinColumn(name = "genre_id")
+    private List<Genre> genres;
+
+    @JsonGetter("genres")
+    public List<String> genreGetter() {
+        if(genres != null) {
+            return genres.stream()
+                    .map(genre -> {
+                        return "/api/vi/genres/" + genre.getGenreId();
+                    }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    @Column(name = "movie_picture_src")
+    private String moviePictureSrc;
+
+    @Column(name = "trailer_uri")
+    private String trailerURI;
+
+    @ManyToOne
+    @JoinColumn(name = "franchise_id")
+    private Franchise franchise;
+
+    @JsonGetter("franchise")
+    public String franchise() {
+        if(franchise != null) {
+            return "/api/v1/franchise/" + franchise.getId();
+        } else {
+            return null;
+        }
+    }
+
+    @ManyToMany(mappedBy = "movies")
+    private List<Character> characters;
+
+    @JsonGetter("characters")
+    public List<String> charactersGetter(){
+        if(characters != null){
+            return characters.stream()
+                    .map(character -> {
+                        return "/api/v1/characters/" + character.getId();
+                    }).collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    public Movie() {
+    }
+
+    public Movie(String movieTitle, String director, int releaseYear, List<Genre> genres, String moviePictureSrc, String trailerURI) {
+        this.movieTitle = movieTitle;
+        this.director = director;
+        this.releaseYear = releaseYear;
+        this.genres = genres;
+        this.moviePictureSrc = moviePictureSrc;
+        this.trailerURI = trailerURI;
+    }
+
+    // Getter and setters
+    public long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(long id) {
+        this.movieId = id;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String title) {
+        this.movieTitle = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public String getMoviePictureSrc() {
+        return moviePictureSrc;
+    }
+
+    public void setMoviePictureSrc(String pictureSrc) {
+        this.moviePictureSrc = pictureSrc;
+    }
+
+    public String getTrailerURI() {
+        return trailerURI;
+    }
+
+    public void setTrailerURI(String trailerURI) {
+        this.trailerURI = trailerURI;
+    }
+}
