0df5e8c192a0461912fa4dd3cee2a19fe9093e92
veliValentine <nikkevanukas1995@gmail.com>
2021-02-10 16:46:36 +0200

    feat: MovieController CRUD

--- src/main/java/movie/api/controllers/MovieController.java
+++ src/main/java/movie/api/controllers/MovieController.java
@@ -33,10 +33,43 @@ public class MovieController {
     }
 
     // get movie by Id
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable long id) {
+        HttpStatus status;
+        Movie movie = new Movie();
+        if (movieRepository.existsById(id)) {
+            movie = movieRepository.findById(id).get();
+            status = HttpStatus.OK;
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(movie, status);
+    }
 
     // Modify movie by Id
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Movie> modifyMovie(@PathVariable long id, @RequestBody Movie newMovie) {
+        Movie returnMovie = new Movie();
+        HttpStatus status;
+        if (id != newMovie.getId()) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(returnMovie, status);
+        }
+        returnMovie = movieRepository.save(newMovie);
+        status = HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(returnMovie, status);
+    }
 
     // Delete movie by id
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Movie> deleteMovie(@PathVariable long id) {
+        if (movieRepository.existsById(id)) {
+            movieRepository.deleteById(id);
+        }
+        HttpStatus status = HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(null, status);
+    }
+
 
     // get all characters in a movie
 }
