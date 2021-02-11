0d52941c01c829de2a4687f5c91811fe47b33d71
veliValentine <nikkevanukas1995@gmail.com>
2021-02-11 14:10:15 +0200

    merge: resolved mergeConflicts

---
+++ src/main/java/movie/api/controllers/HealthController.java
@@ -0,0 +1,17 @@
+package movie.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = ControllerHelper.BASE_URI_V1)
+public class HealthController {
+
+    @GetMapping(value = "/health")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+}
