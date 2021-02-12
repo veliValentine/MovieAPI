79bd383c7b072d11103b3f4d927e1344c3f8439e
veliValentine <nikkevanukas1995@gmail.com>
2021-02-12 11:26:57 +0200

    feat: find one

--- src/main/java/movie/api/controllers/FranchiseController.java
+++ src/main/java/movie/api/controllers/FranchiseController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import static movie.api.controllers.ControllerHelper.BASE_URI_V1;
+import static movie.api.controllers.ControllerHelper.equalIds;
 
 @RestController
 @RequestMapping(value = BASE_URI_V1 + "/franchise")
@@ -30,5 +31,24 @@ public class FranchiseController {
         Franchise franchise = franchiseRepository.save(newFranchise);
         HttpStatus status = HttpStatus.CREATED;
         return new ResponseEntity<>(franchise, status);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Franchise> getFranchise(@PathVariable long id) {
+        Franchise franchise = findFranchiseById(id);
+        HttpStatus status;
+        if (equalIds(franchise.getId(), id)) {
+            status = HttpStatus.OK;
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(franchise, status);
+    }
+
+    private Franchise findFranchiseById(long id) {
+        if (franchiseRepository.existsById(id)) {
+            return franchiseRepository.findById(id).get();
+        }
+        return new Franchise();
     }
 }
